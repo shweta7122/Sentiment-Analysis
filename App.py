3235c5f0abe570dadc960c96770793221057c621
shweta7122 <82398679+shweta7122@users.noreply.github.com>
2021-10-08 20:41:01 -0400

    Update App.py

--- App.py
+++ App.py
@@ -114,7 +114,7 @@ def main():
     if len(TwitterHandle) > 0 :
         # Call the function to extract the data. pass the Twitter Handle and filename you want the data to be stored in.
         with st.spinner("Please wait, Tweets are being extracted"):
-            get_tweets(TwitterHandle , Count=200)
+            get_tweets(TwitterHandle ,count=100)
         st.success('Tweets have been Extracted !!!!')    
            
     
