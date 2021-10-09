d8fc303f07adca0f9f38f6368e12fcc5b127c4be
shweta7122 <82398679+shweta7122@users.noreply.github.com>
2021-10-09 07:17:57 -0400

    Update App.py

--- App.py
+++ App.py
@@ -98,7 +98,7 @@ def main():
     
     def getAnalysis(score):
 	
-        score = convert(score, int)
+        score = int(score)
         if score > 0:
             return 'Positive'
         elif score == 0:
